71ea19ebcefe87ff0f559f19e6c39570a46e26e2
CrWillson <calebwillson1@gmail.com>
2023-04-25 23:48:57 -0400

    Finished project

--- src/termproject/TwoFourTree.java
+++ src/termproject/TwoFourTree.java
@@ -1,6 +1,8 @@
 package termproject;
 
 import java.io.InvalidObjectException;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Title:        Term Project 2-4 Trees
@@ -41,10 +43,12 @@ public class TwoFourTree implements Dictionary {
      * @param key to be searched for
      * @return object corresponding to key; null if not found
      */
-    public Object findElement(Object key) throws InvalidObjectException {
+    public Object findElement(Object key) {
+        /*
         if (!treeComp.isComparable(key)) {
             throw new InvalidObjectException("Invalid key given");
         }
+        */
 
         TFNode currNode = root();
 
@@ -68,6 +72,73 @@ public class TwoFourTree implements Dictionary {
      * @param element to be inserted
      */
     public void insertElement(Object key, Object element) {
+        TFNode currNode = root();
+
+        if (root() == null) {
+            setRoot(new TFNode());
+            root().addItem(0, new Item(key, element));
+        }
+        else {
+            int nextInd = findFirstGreaterThanOrEqual(currNode, key);
+            while (currNode.getChild(nextInd) != null) {
+                currNode = currNode.getChild(nextInd);
+                nextInd = findFirstGreaterThanOrEqual(currNode, key);
+            }
+
+            currNode.insertItem(nextInd, new Item(key, element));
+
+            if (currNode.getNumItems() > 3) {
+                fixOverflow(currNode);
+            }
+        }
+        
+        size++;
+    }
+
+    /**
+     * Helper method to fix overflow
+     * @param node - the overflowed node
+     */
+    private void fixOverflow(TFNode node) {
+        if (node.getParent() == null) {
+            TFNode newRoot = new TFNode();
+            node.setParent(newRoot);
+            newRoot.setChild(0, node);
+            setRoot(newRoot);
+
+            splitNode(node, 0);
+        }
+        else {
+            int parentIndex = whichChild(node);
+            TFNode parent = node.getParent();
+
+            splitNode(node, parentIndex);
+
+            if (parent.getNumItems() > 3) {
+                fixOverflow(parent);
+            }
+        }
+    }
+
+    private void splitNode(TFNode node, int parentInd) {
+        TFNode split = new TFNode();
+
+        split.setParent(node.getParent());
+        split.insertItem(0, node.deleteItem(3));
+        node.getParent().insertItem(parentInd, node.deleteItem(2));
+        node.getParent().setChild(parentInd + 1, split);
+
+        split.setChild(0, node.getChild(3));
+        split.setChild(1, node.getChild(4));
+        node.setChild(3, null);
+        node.setChild(4, null);
+
+        if (split.getChild(0) != null) {
+            split.getChild(0).setParent(split);
+        }
+        if (split.getChild(1) != null) {
+            split.getChild(1).setParent(split);
+        }
     }
 
     /**
@@ -113,72 +184,215 @@ public class TwoFourTree implements Dictionary {
      * @exception ElementNotFoundException if the key is not in dictionary
      */
     public Object removeElement(Object key) throws ElementNotFoundException {
-        return null;
+        TFNode currNode = root();
+        int currInd = -1;
+
+        boolean found = false;
+        while (currNode != null && !found) {
+            for (currInd = 0; currInd < currNode.getNumItems(); currInd++) {
+                if (treeComp.isEqual(currNode.getItem(currInd).key(), key)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) { break; }
+
+            int nextInd = findFirstGreaterThanOrEqual(currNode, key);
+            currNode = currNode.getChild(nextInd);
+        }
+        if (currNode == null) {
+            throw new ElementNotFoundException("Key not found");
+        } 
+
+
+        Object removedElement = null;
+        if (currNode.getChild(0) == null) {
+            removedElement = currNode.removeItem(currInd).element();
+
+            if (currNode.getNumItems() == 0) {
+                fixUnderflow(currNode);
+            }
+        }
+        else {
+            TFNode successor = currNode.getChild(currInd + 1);
+            while (successor != null && successor.getChild(0) != null) {
+                successor = successor.getChild(0);
+            }
+
+            removedElement = currNode.deleteItem(currInd).element();
+            currNode.addItem(currInd, successor.removeItem(0));
+
+            if (successor.getNumItems() == 0) {
+                fixUnderflow(successor);
+            }
+        }
+
+        return removedElement;
+    }
+
+    private void fixUnderflow(TFNode node) {
+        if (node != root()) {
+            int parentInd = whichChild(node);
+            TFNode parent = node.getParent();
+            TFNode leftSib = (parentInd > 0) ? parent.getChild(parentInd - 1) : null;
+            TFNode rightSib = (parentInd < 3) ? parent.getChild(parentInd + 1) : null;
+
+            // left transfer
+            if (leftSib != null && leftSib.getNumItems() > 1) {
+                node.insertItem(0, parent.deleteItem(parentInd - 1));
+
+                node.setChild(0, leftSib.getChild(leftSib.getNumItems()));
+                if (leftSib.getChild(leftSib.getNumItems()) != null) {
+                    leftSib.getChild(leftSib.getNumItems()).setParent(node);
+                }
+
+                leftSib.setChild(leftSib.getNumItems(), null);
+                parent.addItem(parentInd - 1, leftSib.deleteItem(leftSib.getNumItems() - 1));
+            }
+            // right transfer
+            else if (rightSib != null && rightSib.getNumItems() > 1) {
+                node.insertItem(0, parent.deleteItem(parentInd));
+
+                node.setChild(1, rightSib.getChild(0));
+                if (rightSib.getChild(0) != null) {
+                    rightSib.getChild(0).setParent(node);
+                }
+
+                parent.addItem(parentInd, rightSib.removeItem(0));
+            }
+            // left fusion
+            else if (leftSib != null) {
+                TFNode lostChild = node.getChild(0);
+
+                leftSib.insertItem(leftSib.getNumItems(), parent.removeItem(parentInd - 1));
+                leftSib.setChild(leftSib.getNumItems(), lostChild);
+                if (lostChild != null) {
+                    lostChild.setParent(leftSib);
+                }
+                parent.setChild(parentInd - 1, leftSib);
+
+                if (parent.getNumItems() == 0) {
+                    fixUnderflow(parent);
+                }
+            }
+            // right fusion
+            else {
+                TFNode lostChild = node.getChild(0);
+
+                rightSib.insertItem(0, parent.removeItem(parentInd));
+                rightSib.setChild(0, lostChild);
+                if (lostChild != null) {
+                    lostChild.setParent(rightSib);
+                }
+                parent.setChild(parentInd, rightSib);
+
+                if (parent.getNumItems() == 0) {
+                    fixUnderflow(parent);
+                }
+            }
+        }
+        else {
+            setRoot(root().getChild(0));
+            if (root() != null) {
+                root().setParent(null);
+            }
+        }
     }
 
     public static void main(String[] args) {
         Comparator myComp = new IntegerComparator();
         TwoFourTree myTree = new TwoFourTree(myComp);
-
+/*
         myTree.insertElement(47, 47);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(83, 83);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(22, 22);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(16, 16);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(49, 49);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(100, 100);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(38, 38);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(3, 3);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(53, 53);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(66, 66);
-        //myTree.printAllElements();
+        myTree.printAllElements();
         
         myTree.insertElement(19, 19);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(23, 23);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(24, 24);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(88, 88);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(1, 1);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(97, 97);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(94, 94);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(35, 35);
-        //myTree.printAllElements();
+        myTree.printAllElements();
 
         myTree.insertElement(51, 51);
-        //myTree.printAllElements();
+        myTree.printAllElements();
+        myTree.checkTree();
+
+        Object test1 = myTree.findElement(66);
+        Object test2 = myTree.findElement(39);
+        Object test3 = myTree.findElement(22);
+        Object test4 = myTree.findElement(52);
+        Object test5 = myTree.findElement(47);
 
         myTree.printAllElements();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int input = scan.nextInt();
+
+        while (input != -1) {
+            myTree.removeElement(input);
+            myTree.printAllElements();
+
+            System.out.println("Input number: ");
+            input = scan.nextInt();
+        }
+
+*/
+/*
+        Random rng = new Random();
+
+        for (int i = 0; i < 25; i++) {
+            int rand = rng.nextInt(50);
+            myTree.insertElement(rand, rand);
+        }
+        myTree.printAllElements();
+        myTree.checkTree();
         System.out.println("done");
+/* */
 
         myTree = new TwoFourTree(myComp);
         final int TEST_SIZE = 10000;
@@ -195,11 +409,12 @@ public class TwoFourTree implements Dictionary {
             if (out != i) {
                 throw new TwoFourTreeException("main: wrong element removed");
             }
-            if (i > TEST_SIZE - 15) {
+            //if (i > TEST_SIZE - 15) {
                 myTree.printAllElements();
-            }
+            //}
         }
         System.out.println("done");
+
     }
 
     public void printAllElements() {
